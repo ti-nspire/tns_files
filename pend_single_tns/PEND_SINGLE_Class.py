ae996d3210da8835ee27e6328d9fc2d58c26d9e3
ti-nspire <29220357+ti-nspire@users.noreply.github.com>
2020-09-24 12:01:39 +0900

    Add files via upload

---
+++ pend_single_tns/PEND_SINGLE_Class.py
@@ -0,0 +1,104 @@
+import RK4_Class
+import time
+import ti_system as tsys
+import ti_draw as td
+from math import sin as SIN
+from math import cos as COS
+from math import pi as PI
+
+class PEND_SINGLE_Class:
+  def __init__(self, radius, init_radian, init_vel, h, div):
+    td.use_buffer()
+
+    # 引数を定数としてとっておく。リセット時に使う。
+    self.RADIUS = radius
+    self.RADIAN = init_radian
+    self.VEL = init_vel
+    self.ARGs = [self.RADIUS, self.RADIAN, self.VEL, h, div]
+
+    # 画面のx軸、y軸の範囲を設定する。
+    # ここではx軸を-1.65～1.65、y軸を-1.1～1.1にした。
+    self.WIDTH, self.HIGHT = td.get_screen_dim()
+    ASPECT = self.WIDTH / self.HIGHT
+    self.Y_MIN, self.Y_MAX = -radius * 1.1, radius * 1.1
+    self.X_MIN, self.X_MAX = self.Y_MIN * ASPECT, self.Y_MAX * ASPECT
+    td.set_window(self.X_MIN, self.X_MAX, self.Y_MIN, self.Y_MAX)
+
+    # 質点の描画半径。シミュレーションとは無関係。
+    self.MASS_R = 0.1
+
+    #更新すべき初期値を設定する。
+    self.t0 = 0    
+    self.radian = init_radian
+    self.vel = init_vel
+    self.inits = [init_radian, init_vel]
+
+    # 解くべき微分方程式を定義する。
+    def dotRadian(t, radian, vel):
+      return vel
+    def dotVel(t, radian, vel):
+      return (-9.80665 / self.RADIUS) * SIN(radian)
+    self.FUNCS = [dotRadian, dotVel]
+
+    # 上記の微分方程式を上記の初期値で解くための函数を実体化する。
+    self.rk4 = RK4_Class.RK4_Class(self.FUNCS, self.inits, h, div)
+
+  def update(self):
+    self.rk4.update()
+    self.t0 = self.rk4.t0
+    self.radian = self.rk4.inits[0]
+    self.vel = self.rk4.inits[1]
+
+  def draw(self):
+    td.clear()
+    
+    x = self.RADIUS * SIN(self.radian)
+    y = self.RADIUS * -COS(self.radian)
+    td.draw_line(0, 0, x, y)
+    td.draw_circle(x, y, self.MASS_R)
+    
+    td.draw_text(self.X_MIN, self.Y_MAX-0.2, "radius: "+str(self.RADIUS)+" m")
+    td.draw_text(self.X_MIN, self.Y_MAX-0.3, "sec: "+str(self.t0))
+    td.draw_text(self.X_MIN, self.Y_MAX-0.4, "radian: "+str(self.radian))
+    td.draw_text(self.X_MIN, self.Y_MIN+0.15, "To reset, press the esc key.")
+    td.draw_text(self.X_MIN, self.Y_MIN+0, "To pause or resume, press the enter key.")
+
+    td.paint_buffer()
+
+  def update_and_draw(self):
+    self.update()
+    self.draw()
+
+  def start_sim(self):
+    while tsys.get_key() != "enter":
+      pass
+
+  def reset(self):
+    self.__init__(*self.ARGs)
+    self.draw()
+    
+  def pause_or_resume(self):
+    while True:
+      key = tsys.get_key()
+      if key == "esc":
+        self.reset()
+      if key == "enter":
+        break
+
+def main():
+  # 引数は(半径, 初期角度, 初期角速度, ステップ, 1ステップの内部分割数)
+  pend_single = PEND_SINGLE_Class(1, PI * 0.999 , 0, 1/2**4, 8)
+  pend_single.draw()
+  pend_single.start_sim()
+
+  while True:
+    pend_single.update_and_draw()
+    
+    key = tsys.get_key()
+    if key == "enter":
+      pend_single.pause_or_resume()
+    if key == "esc":
+      pend_single.reset()
+
+    #time.sleep_ms(20)
+
---
+++ pend_single_tns/RK4_Class.py
@@ -0,0 +1,58 @@
+# Python による常微分方程式の数値解法_改良版 / 古典的 Runge-Kutta 法
+class RK4_Class:
+  def __init__(self, funcs, inits, h, numOfDiv=1):
+    self.funcs = funcs
+    self.t0 = 0
+    self.inits = inits
+    self.h = h / numOfDiv
+    self.half_h = self.h / 2
+    self.sixth_h = self.h / 6
+    self.range_dim = range(len(funcs))
+    self.range_div = range(numOfDiv)
+    self.f = [[0 for i in self.range_dim] for i in range(4)]
+    self.temp = [[0 for i in self.range_dim] for i in range(3)]
+
+  def update(self):
+    for i in self.range_div:
+      self.f[0] = [self.funcs[j](self.t0, *self.inits) for j in self.range_dim]
+      self.temp[0] = [self.inits[j] + self.half_h * self.f[0][j] for j in self.range_dim]
+      self.f[1] = [self.funcs[j](self.t0 + self.half_h, *self.temp[0]) for j in self.range_dim]
+      self.temp[1] = [self.inits[j] + self.half_h * self.f[1][j] for j in self.range_dim]
+      self.f[2] = [self.funcs[j](self.t0 + self.half_h, *self.temp[1]) for j in self.range_dim]
+      self.temp[2] = [self.inits[j] + self.h * self.f[2][j] for j in self.range_dim]
+      self.f[3] = [self.funcs[j](self.t0 + self.h , *self.temp[2]) for j in self.range_dim]
+      self.inits = [self.inits[j] + self.sixth_h * (self.f[0][j] + 2 * (self.f[1][j] + self.f[2][j]) + self.f[3][j]) for j in self.range_dim]
+      self.t0 += self.h
+    return self
+
+  def print(self):
+    print(self.t0, self.inits)
+    return self
+
+########
+# test #
+########
+#if __name__ == "__main__":
+def main():
+  # 解くべき連立常微分方程式を定義する。
+  # 確認のため、厳密解の存在する方程式を定義する。単振り子とは無関係。
+  def xDot(t, x, y): # x'(t) = y(t)
+    return y
+  def yDot(t, x, y): # y'(t) = t - x(t)
+    return t - x
+  funcs = [xDot, yDot]
+
+  # 従属変数の初期値を指定する。
+  x0 = 0
+  y0 = 0
+  inits = [x0, y0]
+
+  # ステップ幅を指定する。
+  h = 0.25 # =1/2**2
+  
+  # 1ステップだけ計算する函数を実体化して、
+  rk4 = RK4_Class(funcs, inits, h)
+
+  # 初期値を何度か更新して確認する。
+  while rk4.t0 < 7.5:
+    rk4.update().print()
