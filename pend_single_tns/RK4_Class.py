6d767e731c73c5b83c6f6ea0e2f9fd1a812842e3
ti-nspire <29220357+ti-nspire@users.noreply.github.com>
2021-10-15 08:36:06 +0900

    Update RK4_Class.py

--- pend_single_tns/RK4_Class.py
+++ pend_single_tns/RK4_Class.py
@@ -3,27 +3,28 @@ class RK4_Class:
   def __init__(self, funcs, inits, h, num_of_div=1):
     _num_of_funcs = len(funcs)
 
-    self.funcs = funcs
-    self.t0 = 0
-    self.inits = inits
-    self.h = h / num_of_div
-    self.half_h = self.h / 2.0
-    self.sixth_h = self.h / 6.0
+    self.funcs     = funcs
+    self.t0        = 0
+    self.inits     = inits
+    self.h         = h / num_of_div
+    self.half_h    = self.h / 2.0
+    self.sixth_h   = self.h / 6.0
     self.range_dim = range(_num_of_funcs)
     self.range_div = range(num_of_div)
-    self.f = [[0.0] * _num_of_funcs] * 4
-    self.temp = [[0.0] * _num_of_funcs] * 3
+    self.f         = [[0.0] * _num_of_funcs] * 4
+    self.temp      = [[0.0] * _num_of_funcs] * 3
 
   def update(self):
     for i in self.range_div:
-      self.f[0] = [self.funcs[j](self.t0, *self.inits) for j in self.range_dim]
+      self.f[0]    = [self.funcs[j](self.t0, *self.inits) for j in self.range_dim]
       self.temp[0] = [self.inits[j] + self.half_h * self.f[0][j] for j in self.range_dim]
-      self.f[1] = [self.funcs[j](self.t0 + self.half_h, *self.temp[0]) for j in self.range_dim]
+      self.f[1]    = [self.funcs[j](self.t0 + self.half_h, *self.temp[0]) for j in self.range_dim]
       self.temp[1] = [self.inits[j] + self.half_h * self.f[1][j] for j in self.range_dim]
-      self.f[2] = [self.funcs[j](self.t0 + self.half_h, *self.temp[1]) for j in self.range_dim]
+      self.f[2]    = [self.funcs[j](self.t0 + self.half_h, *self.temp[1]) for j in self.range_dim]
       self.temp[2] = [self.inits[j] + self.h * self.f[2][j] for j in self.range_dim]
-      self.f[3] = [self.funcs[j](self.t0 + self.h , *self.temp[2]) for j in self.range_dim]
-      self.inits = [self.inits[j] + self.sixth_h * (self.f[0][j] + 2.0 * (self.f[1][j] + self.f[2][j]) + self.f[3][j]) for j in self.range_dim]
+      self.f[3]    = [self.funcs[j](self.t0 + self.h , *self.temp[2]) for j in self.range_dim]
+      self.inits   = [self.inits[j] + self.sixth_h * (self.f[0][j] + 2.0 * (self.f[1][j] + self.f[2][j]) + self.f[3][j]) for j in self.range_dim]
+
       self.t0 += self.h
     return self
 
@@ -38,10 +39,8 @@ class RK4_Class:
 def main():
   # 解くべき連立常微分方程式を定義する。
   # 確認のため、厳密解の存在する方程式を定義する。単振り子とは無関係。
-  def xDot(t, x, y): # x'(t) = y(t)
-    return y
-  def yDot(t, x, y): # y'(t) = t - x(t)
-    return t - x
+  def xDot(t, x, y): return y      # x'(t) = y(t)
+  def yDot(t, x, y): return t - x  # y'(t) = t - x(t)
   funcs = [xDot, yDot]
 
   # 従属変数の初期値を指定する。
